f9d15862fb75032d501e6c212f234819787eaf6b
yvind Helgeland <oyvihel@compute-1-24.local>
2016-05-06 12:48:54 +0200

    Removed duplicate files

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module load bowtie
-
-cp -r /work/projects/norbis/workflows/chip/data/ .
-cp -r /work/projects/norbis/workflows/chip/programs .
-chmod a+rx programs/*
-bowtie-build -f data/saci.fasta data/saci
