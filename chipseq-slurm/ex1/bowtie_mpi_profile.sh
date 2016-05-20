241a55e321ea738258dc402654f9990a8586766a
Øyvind Helgeland <helgeland.oyvind@gmail.com>
2016-05-20 23:58:46 +0200

    Added change to bowtie profile

--- chipseq-slurm/ex1/bowtie_mpi_profile.sh
+++ chipseq-slurm/ex1/bowtie_mpi_profile.sh
@@ -2,7 +2,8 @@
 
 runpar() {
 	mpirun -np $1 pmap_dist . . data/N1_CHIP.fastq -i data/ saci >/dev/null 2>&1
-        mpirun -np $1 pmap -i data/ saci . . bowtie -S -v 2 -m 1 >/dev/null 2>&1
+	mpirun -np $1 pmap -i data/ saci . . bowtie -S -v 2 -m 1 >/dev/null 2>&1
+	awk 'NR <= 3 || !/^@/' < out.txt > N1_CHIP.sam
 }
 
 
