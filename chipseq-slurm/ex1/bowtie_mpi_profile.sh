3b86f962d4b02757504e7565b6eda8ef958bcbe0
yvind Helgeland <oyvihel@compute-1-32.local>
2016-05-21 00:12:07 +0200

    Change bowtie_mpi_profile.sh to work with new folder structure

--- chipseq-slurm/ex1/bowtie_mpi_profile.sh
+++ chipseq-slurm/ex1/bowtie_mpi_profile.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 runpar() {
-	mpirun -np $1 pmap_dist . . data/N1_CHIP.fastq -i data/ saci >/dev/null 2>&1
-	mpirun -np $1 pmap -i data/ saci . . bowtie -S -v 2 -m 1 >/dev/null 2>&1
+	mpirun -np $1 pmap_dist . . ../data/N1_CHIP.fastq -i ../data/ saci
+	mpirun -np $1 pmap -i ../data/ saci . . bowtie -S -v 2 -m 1
 	awk 'NR <= 3 || !/^@/' < out.txt > N1_CHIP.sam
 }
 
